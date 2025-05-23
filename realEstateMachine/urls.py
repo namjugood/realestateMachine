94f26ccd97d483114f659c8e3c86364a92e7b17c
namjugood <namjugood@naver.com>
2025-05-23 23:59:11 +0900

    django 기초 세팅

---
+++ realEstateMachine/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for realEstateMachine project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# URL 라우팅 설정
+# 루트 URL에 대한 요청이 들어오면 views.index 함수를 호출하여 응답
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
---
+++ run.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import django
+from django.core.management import execute_from_command_line
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# Django 프로젝트 경로 설정
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realEstateMachine.settings')
+django.setup()
+
+class DjangoReloader(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path.endswith('.py'):
+            print(f"File changed: {event.src_path}")
+            print("Reloading Django server...")
+            os.execv(sys.executable, ['python'] + sys.argv)
+
+if __name__ == "__main__":
+    print("Starting Django development server with auto-reload...")
+    event_handler = DjangoReloader()
+    observer = Observer()
+    observer.schedule(event_handler, path='.', recursive=True)
+    observer.start()
+    
+    try:
+        execute_from_command_line(['manage.py', 'runserver'])
+    except KeyboardInterrupt:
+        observer.stop()
+        print("\nStopping server...")
+    observer.join() 
